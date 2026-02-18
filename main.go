59e3fb895403090640fc3e8b8ffd38b6f01d8f55
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-02-18 16:43:15 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("port is not found in the environment")
+	}
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on Port: %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
