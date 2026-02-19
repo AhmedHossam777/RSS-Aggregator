59ba326cf52b04d997325c9659705dc585484c8b
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-02-19 06:25:59 +0200

    i

--- main.go
+++ main.go
@@ -4,41 +4,50 @@ import (
 	"log"
 	"net/http"
 	"os"
-
+	
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	godotenv.Load()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Failed to load the dotenv file")
+	}
+	
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("port is not found in the environment")
 	}
-
+	
 	router := chi.NewRouter()
-	router.Use(cors.Handler(cors.Options{
-		MaxAge:           300,
-		AllowedOrigins:   []string{"https://*", "http://*"},
-		AllowedMethods:   []string{"GET", "POST", "PATCH", "PUT", "DELETE"},
-		AllowedHeaders:   []string{"Link"},
-		AllowCredentials: false,
-	}))
-
+	router.Use(
+		cors.Handler(
+			cors.Options{
+				MaxAge:           300,
+				AllowedOrigins:   []string{"https://*", "http://*"},
+				AllowedMethods:   []string{"GET", "POST", "PATCH", "PUT", "DELETE"},
+				AllowedHeaders:   []string{"Link"},
+				AllowCredentials: false,
+			},
+		),
+	)
+	
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", HandlerReadiness)
 	v1Router.Get("/err", HandlerErr)
-
+	
 	router.Mount("/v1", v1Router)
-
+	
 	srv := &http.Server{
 		Handler: router,
 		Addr:    ":" + portString,
 	}
-
+	
 	log.Printf("Server starting on Port: %v", portString)
-	err := srv.ListenAndServe()
+	
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
